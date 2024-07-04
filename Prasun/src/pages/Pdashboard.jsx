8458f09ce8dab9905e8855b7d5a4e15f1a5178ff
prasun <prasun.1928@gmail.com>
2024-07-04 16:42:38 +0530

    added patient dashboard

--- Prasun/src/pages/Pdashboard.jsx
+++ Prasun/src/pages/Pdashboard.jsx
@@ -6,8 +6,31 @@ function Pdashboard() {
   return (
     <>
     <Header />
+    <div className='flex justify-between'>
+      <div className='bg-slate-300 w-36 rounded-md shadow-lg'>
+        <h2>blood group</h2>
+         <p className='bg-slate-200'>O+</p>
+        </div>
+      <div className='bg-slate-300 w-36 rounded-md shadow-lg'>
+        <h2>Age</h2>
+        <p  className='bg-slate-200'>23</p>
+        </div>
+      <div className='bg-slate-300 w-36 rounded-md shadow-lg'>
+        <h2>Gender</h2>
+        <p  className='bg-slate-200'>M</p>
+        </div>
+      <div className='bg-slate-300 w-36 rounded-md shadow-lg'>
+        <h2>height</h2>
+        <p  className='bg-slate-200'>179 cm</p>
+        </div>
+      <div className='bg-slate-300 w-36 rounded-md shadow-lg'>
+        <h2>weight</h2>
+        <p  className='bg-slate-200'>70 kg</p>
+        </div>
+
+    </div>
     <div className='flex flex-wrap'>
-      <div><Btn label='Blood Group'/></div>
+      
       <div><Btn label='find doctors'/></div>
       <div><Btn label='disease'/></div>
       <div><Btn label='add a prescription'/></div>
