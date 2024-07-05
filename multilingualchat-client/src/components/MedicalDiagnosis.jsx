30a563b74a8aed7defc9bb7f2b98e122e0b6a9ad
mritunjay8 <a34mritunjaysingh@gmail.com>
2024-07-05 09:29:53 +0530

    deployment link update

--- multilingualchat-client/src/components/MedicalDiagnosis.jsx
+++ multilingualchat-client/src/components/MedicalDiagnosis.jsx
@@ -19,7 +19,7 @@ const MedicalDiagnosis = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/api/medical/diagnose', {
+      const response = await fetch('https://recursive-innon8-2.onrender.com/api/medical/diagnose', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
